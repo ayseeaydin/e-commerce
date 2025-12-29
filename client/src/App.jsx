c5c0eb9b76164c3529eeca63b050f68819f0736b
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-12-29 19:07:48 +0300

    add Header, Footer, and Policy components with styling and update App component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Policy from './components/Policy';
+import Footer from './components/layout/Footer/Footer';
+import Header from './components/layout/Header/Header';
+import Policy from './components/layout/Policy/Policy';
 
 function App() {
   return (
