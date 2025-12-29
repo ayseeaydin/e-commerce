7b9f0a5dc03576438fb9be673ea1444f85a7d5fe
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-12-29 19:48:44 +0300

    add Categories component with CategoryItem and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Categories from './components/Categories/Categories';
 import Footer from './components/layout/Footer/Footer';
 import Header from './components/layout/Header/Header';
 import Policy from './components/layout/Policy/Policy';
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <Sliders />
+      <Categories />
       <Policy />
       <Footer />
     </div>
