ec5fe305adc63fdc5826e7b15f8581a28dbbe7bf
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-12-31 14:33:55 +0300

    add CampaignSingle component with styling

---
+++ client/src/components/CampaignSingle/CampaignSingle.jsx
@@ -0,0 +1,21 @@
+import "./CampaignSingle.css";
+
+const CampainSingle = () => {
+    return (
+        <section class="campaign-single">
+            <div class="container">
+                <div class="campaign-wrapper">
+                    <h2>New Season Sale</h2>
+                    <strong>40% OFF</strong>
+                    <span></span>
+                    <a href="#" class="btn btn-lg">
+                        SHOP NOW
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CampainSingle;
