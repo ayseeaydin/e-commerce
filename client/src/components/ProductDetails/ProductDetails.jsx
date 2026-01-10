b65aaea54be58d4f238b45122a3fd9ad41857ce2
ayseeaydin <ayssee.aydinn.04@gmail.com>
2026-01-11 00:29:07 +0300

    add Gallery component for product image display; integrate into ProductDetails

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import Breadcrumb from "./Breadcrumb/Breadcrumb";
+import Gallery from "./Gallery/Gallery";
 import "./ProductDetails.css";
 
 const ProductDetails = () => {
@@ -9,24 +10,7 @@ const ProductDetails = () => {
                     <Breadcrumb />
                     <div className="single-content">
                         <main className="site-main">
-                            <div className="product-gallery">
-                                <div className="single-image-wrapper">
-                                    <img src="img/products/product2/1.png" id="single-image" alt="" />
-                                </div>
-                                <div className="product-thumb">
-                                    <div className="glide__track" data-glide-el="track">
-                                        <ol className="gallery-thumbs glide__slides"></ol>
-                                    </div>
-                                    <div className="glide__arrows" data-glide-el="controls">
-                                        <button className="glide__arrow glide__arrow--left" data-glide-dir="<">
-                                            <i className="bi bi-chevron-left"></i>
-                                        </button>
-                                        <button className="glide__arrow glide__arrow--right" data-glide-dir=">">
-                                            <i className="bi bi-chevron-right"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
+                            <Gallery/>
                             <div className="product-info">
                                 <h1 className="product-title">
                                     Ridley High Waist
